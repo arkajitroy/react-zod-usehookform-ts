c1a8ca65862f85efad0ced35b46b871f4606466a
arkajitroy <arkajitroy11.official@gmail.com>
2024-06-29 11:35:23 +0530

    feat: add initial files + configuration

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h3 className="text-blue-900 font-semibold">React Form use-hook-form + zod</h3>;
+    </div>
+  );
+};
+
+export default App;
