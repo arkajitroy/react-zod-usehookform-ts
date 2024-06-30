ac63a2ee25189c06ceae6bd2a0222997496e2790
arkajitroy <arkajitroy11.official@gmail.com>
2024-06-30 12:32:50 +0530

    feat: add new-user functionality

--- src/components/pages/Users.tsx
+++ src/components/pages/Users.tsx
@@ -1,6 +1,6 @@
 import React, { Fragment, useEffect } from "react";
 import { useFieldArray, useFormContext, useWatch } from "react-hook-form";
-import { Button, Stack } from "@mui/material";
+import { Button, Container, Stack } from "@mui/material";
 import { TSchema } from "../../@types/schemas/ZSchemas";
 import {
   RHFAutoComplete,
@@ -13,9 +13,10 @@ import {
   RHFToggleButtonGroup,
 } from "..";
 import { useGetGenders, useGetLanguages, useGetSkills, useGetStates } from "../../services/queries";
+import { formDefaultValues } from "../../constants/defaultValues";
 
 const Users: React.FC = () => {
-  const { watch, control, unregister } = useFormContext<TSchema>();
+  const { watch, control, unregister, reset } = useFormContext<TSchema>();
   // field-array-functionality
   const { append, fields, remove, replace } = useFieldArray({
     control,
@@ -29,8 +30,12 @@ const Users: React.FC = () => {
   const skillsQueryData = useGetSkills();
   const isTeacherData = useWatch({ control, name: "isTeacher" });
 
+  // handle-change and handle-clicks
+  const handleResetFormClick = () => reset(formDefaultValues);
+
+  // use-effects
   useEffect(() => {
-    const subscribe = watch((value) => console.log("values : ", value));
+    const subscribe = watch((value) => console.log("form-values - watch : ", value));
     return () => subscribe.unsubscribe();
   }, [watch]);
 
@@ -42,30 +47,42 @@ const Users: React.FC = () => {
   }, [isTeacherData, replace, unregister]);
 
   return (
-    <Stack sx={{ gap: 2 }}>
-      <RHFTextField<TSchema> name="name" label="Username" />
-      <RHFTextField<TSchema> name="email" label="Email Address" />
-      <RHFAutoComplete<TSchema> name="states" label="States" options={statesQueryData.data} />
-      <RHFToggleButtonGroup<TSchema> name="languagesSpoken" options={languagesQueryData.data} />
-      <RHFRadioGroup<TSchema> name="gender" label="Gender" options={genderQueryData.data} />
-      <RHFCheckbox<TSchema> name="skills" label="Skills" options={skillsQueryData.data} />
-      <RHFDateTimePicker<TSchema> name="formerEmploymentPeriod" label="Registration Date & Time" />
-      <RHFSlider<TSchema> name="salaryRange" label="Salary Range" />
-      <RHFSwitch<TSchema> name="isTeacher" label="Are you a Teacher" />
-      {isTeacherData && (
-        <Button onClick={() => append({ name: "" })} type="button">
-          Add new student
-        </Button>
-      )}
-      {fields.map((field, index) => (
-        <Fragment key={field.id}>
-          <RHFTextField<TSchema> name={`students.${index}.name`} label={field.name} />
-          <Button type="button" variant="contained" color="error" onClick={() => remove(index)}>
-            Remove
+    <Container maxWidth="sm" component="form">
+      <Stack sx={{ gap: 2 }}>
+        <RHFTextField<TSchema> name="name" label="Username" />
+        <RHFTextField<TSchema> name="email" label="Email Address" />
+        <RHFAutoComplete<TSchema> name="states" label="States" options={statesQueryData.data} />
+        <RHFToggleButtonGroup<TSchema> name="languagesSpoken" options={languagesQueryData.data} />
+        <RHFRadioGroup<TSchema> name="gender" label="Gender" options={genderQueryData.data} />
+        <RHFCheckbox<TSchema> name="skills" label="Skills" options={skillsQueryData.data} />
+        <RHFDateTimePicker<TSchema>
+          name="formerEmploymentPeriod"
+          label="Registration Date & Time"
+        />
+        <RHFSlider<TSchema> name="salaryRange" label="Salary Range" />
+        <RHFSwitch<TSchema> name="isTeacher" label="Are you a Teacher" />
+        {isTeacherData && (
+          <Button onClick={() => append({ name: "" })} type="button">
+            Add new student
           </Button>
-        </Fragment>
-      ))}
-    </Stack>
+        )}
+        {fields.map((field, index) => (
+          <Fragment key={field.id}>
+            <RHFTextField<TSchema> name={`students.${index}.name`} label={field.name} />
+            <Button type="button" variant="contained" color="error" onClick={() => remove(index)}>
+              Remove
+            </Button>
+          </Fragment>
+        ))}
+
+        <Stack sx={{ flexDirection: "row", justifyContent: "space-between" }}>
+          <Button variant="contained" color="warning" type="submit">
+            New User
+          </Button>
+          <Button onClick={handleResetFormClick}>Reset</Button>
+        </Stack>
+      </Stack>
+    </Container>
   );
 };
 
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 import { Option } from "../@types/TInputFields";
 import { LOCAL_SERVER_API_ENDPOINT } from "../config/config";
+import { TSchema } from "../@types/schemas/ZSchemas";
+
+// ======================== GET ========================
 
 export const getAllStatesAPI = () => {
   return axios
@@ -24,4 +27,10 @@ export const getAllSkillsAPI = () => {
   return axios
     .get<Array<Option>>(`${LOCAL_SERVER_API_ENDPOINT}/skills`)
     .then((response) => response.data);
+};
+
+// ============= ADD / POST / PATCH / DELETE ===================
+
+export const addNewUserAPI = async (_payloadData: TSchema) => {
+  await axios.post(`${LOCAL_SERVER_API_ENDPOINT}/users`, _payloadData);
 };
